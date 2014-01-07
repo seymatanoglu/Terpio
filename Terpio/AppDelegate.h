c13672e1aced3201b067993caf65972540f19115
Seyma Tanoglu <stanoglu@gmail.com>
2014-01-07 20:24:43 +0200

    initial commit

---
+++ Terpio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Terpio
+//
+//  Created by Seyma Tanoglu on 1/7/14.
+//  Copyright (c) 2014 Seyma Tanoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TerpioTests/TerpioTests.m
@@ -0,0 +1,34 @@
+//
+//  TerpioTests.m
+//  TerpioTests
+//
+//  Created by Seyma Tanoglu on 1/7/14.
+//  Copyright (c) 2014 Seyma Tanoglu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TerpioTests : XCTestCase
+
+@end
+
+@implementation TerpioTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
